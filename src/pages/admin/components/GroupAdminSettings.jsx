b33486f46ddcba0b7d372da5c4136f225d96a32b
Styrle <jserpis@hotmail.co.uk>
2025-06-25 21:28:57 +0100

    Changes to logo on non groups

--- src/pages/admin/components/GroupAdminSettings.jsx
+++ src/pages/admin/components/GroupAdminSettings.jsx
@@ -21,6 +21,8 @@ import FlexView from "react-flexview";
 import {css} from "aphrodite";
 import {NavLink} from "react-router-dom";
 import 'react-quill/dist/quill.snow.css';
+// React Quill - text editor
+import ReactQuill from 'react-quill';
 
 import {connect} from "react-redux";
 import * as groupAdminSettingsActions from "../../../redux-store/actions/groupAdminSettingsActions";
@@ -31,6 +33,7 @@ import * as colors from "../../../values/colors";
 import * as utils from "../../../utils/utils";
 import * as ROUTES from "../../../router/routes";
 import Routes from "../../../router/routes";
+import ManageSectors from "./manage-sectors/ManageSectors";
 
 const mapStateToProps = state => {
     return {
@@ -105,6 +108,9 @@ class GroupAdminSettings extends Component {
             groupDetails,
             groupAttributesEdited,
 
+            clubAttributes,
+            clubAttributesEdited,
+
             groupWebsite,
             groupDescription,
             primaryColor,
@@ -118,6 +124,7 @@ class GroupAdminSettings extends Component {
             cancelEditingGroupDetails,
             saveColor,
             cancelEditingColor,
+            saveEditedQuill,
         } = this.props;
 
         if (!groupAttributesEdited) {
@@ -439,6 +446,40 @@ class GroupAdminSettings extends Component {
                                         groupDetails.settings.primaryColor
                             }}/>
                     </Col>
+
+                    {/** Edit sectors */}
+                    <Col xs={12} sm={12} md={12} lg={12}>
+                        <ManageSectors/>
+                    </Col>
+
+                    {/** Divider */}
+                    <Col xs={12} sm={12} md={12} lg={12}>
+                        <Divider style={{marginTop: 40, marginBottom: 40, height: 4,
+                                backgroundColor:
+                                    !groupDetails
+                                        ?
+                                        colors.primaryColor
+                                        :
+                                        groupDetails.settings.primaryColor
+                            }}/>
+                    </Col>
+
+                    {/** Edit create project terms and conditions */}
+                    <Col xs={12} sm={12} md={12} lg={12}>
+                        <FlexView column>
+                            <Typography variant="h6" color="primary">Edit create project terms and conditions</Typography>
+
+                            <NavLink to={ROUTES.CREATE_PITCH_TERMS_AND_CONDITIONS} target="_blank" className={css(sharedStyles.nav_link_hover_without_changing_text_color)} style={{marginTop: 30}}>
+                                <Button variant="outlined" color="primary" className={css(sharedStyles.no_text_transform)}>View create project terms and conditions page</Button>
+                            </NavLink>
+
+                            <ReactQuill theme="snow" onChange={this.handleQuillEditorChanged('createPitchTermsAndConditions')} modules={modules} value={clubAttributesEdited && clubAttributesEdited.hasOwnProperty('createPitchTermsAndConditions') ? clubAttributesEdited.createPitchTermsAndConditions : {ops: []}} style={{marginTop: 20}}/>
+
+                            <FlexView marginTop={15} width="100%" hAlignContent="right">
+                                <Button variant="contained" color="primary" onClick={() => saveEditedQuill('createPitchTermsAndConditions')} style={{marginLeft: 12}}>Save</Button>
+                            </FlexView>
+                        </FlexView>
+                    </Col>
 {/* 
                     <Col xs={12} sm={12} md={12} lg={12}>
                         <FlexView column>
@@ -478,7 +519,7 @@ class GroupAdminSettings extends Component {
                             <ReactQuill theme="snow" onChange={this.handleQuillEditorChanged('termsOfUse')} modules={modules} value={clubAttributesEdited && clubAttributesEdited.hasOwnProperty('termsOfUse') ? clubAttributesEdited.termsOfUse : {ops: []}} style={{marginTop: 20 }}/>
 
                             <FlexView marginTop={15} width="100%" hAlignContent="right">
-                                <Button variant="contained" color="primary" onClick={() => saveEditedQuill('termsOfUse')} style={{marginLeft: 12}}>Save</Button>
+                                <Button variant="contained" color="primary" onClick={() => saveEditedQuill('termsOfUe')} style={{marginLeft: 12}}>Save</Button>
                             </FlexView>
                         </FlexView>
                     </Col>
--- src/shared-components/header/Header.tsx
+++ src/shared-components/header/Header.tsx
@@ -22,6 +22,7 @@ import {getGroupLogo} from "../../models/group_properties";
 import Routes from "../../router/routes";
 import {css} from "aphrodite";
 import sharedStyles from "../../shared-js-css-styles/SharedStyles";
+import defaultLogo from "../../img/logo.png";
 
 interface HeaderProps extends HeaderLocalProps {
     MediaQueryState: MediaQueryState;
@@ -118,11 +119,23 @@ class Header extends Component<HeaderProps, {}> {
                                     <img
                                         alt="logo"
                                         src={
-                                            Routes.isErrorRoute(routePath) || Routes.isSystemPublicRoute(routePath)
-                                                ? require("../../img/logo.png").default
-                                                : ManageGroupUrlState.group && getGroupLogo(ManageGroupUrlState.group)
-                                                    ? getGroupLogo(ManageGroupUrlState.group)
-                                                    : require("../../img/logo.png").default
+                                            (() => {
+                                                const isSystemPublic = Routes.isSystemPublicRoute(routePath);
+                                                const isError = Routes.isErrorRoute(routePath);
+                                                console.log('Header Debug:', { routePath, isSystemPublic, isError });
+                                                
+                                                if (isError || isSystemPublic) {
+                                                    console.log('Using system logo:', defaultLogo);
+                                                    return defaultLogo;
+                                                } else if (ManageGroupUrlState.group && getGroupLogo(ManageGroupUrlState.group)) {
+                                                    const groupLogo = getGroupLogo(ManageGroupUrlState.group);
+                                                    console.log('Using group logo:', groupLogo);
+                                                    return groupLogo;
+                                                } else {
+                                                    console.log('Using default logo:', defaultLogo);
+                                                    return defaultLogo;
+                                                }
+                                            })()
                                         }
                                         style={{ width: "auto", height: 36, objectFit: "contain" }}
                                     />
